f0b6cdf9c04c01ce6e04fcc1eec082849356a85c
clementmwamba <clementmwamba23@gmail.com>
2023-11-19 23:41:00 +0300

    3

--- code-challenge-5/src/App.js
+++ code-challenge-5/src/App.js
@@ -1,9 +1,24 @@
-
+import React,{useState,useEffect} from 'react';
 import './App.css';
+import BotCollection from './Components/BotCollection';
+
 
 function App() {
+  const [bots,setbots]= useState ([])
+    
+  useEffect (()=> {
+      fetch ("http://localhost:3000/bots")
+      .then (response => response.json())
+      .then (data => {
+          setbots(data)})
+
+  }, []);
+  console.log(bots)
+
+  
   return (
     <div className="App">
+      <BotCollection/>
     
     </div>
   );
---
+++ code-challenge-5/src/Components/BotCollection.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react'
+
+function BotCollection() {
+
+
+  return (
+    <div>
+        <h4>BotCollection</h4>
+    </div>
+  )
+}
+
+export default BotCollection
